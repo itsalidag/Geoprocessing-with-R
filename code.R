cd9fcc41a106d6985675247f860517d8e27b1eff
itsalidag <61254104+itsalidag@users.noreply.github.com>
2020-11-15 23:22:57 +0300

    Update code.R

--- code.R
+++ code.R
@@ -4,6 +4,21 @@
 # installing rgdal and sp packages.
 library(rgdal)
 #changing the working directory to be same as our shapefiles.
-setwd("D:\kaggle_books\turkey-latest-free.shp")
+setwd("D:\\[2]Atölye 7\\STRATEJİK MEKANSAL PLANLAMA\\ilçe çalışmaları\\Türkiye_İl_İlçe_Shape\\Türkiye_İl_İlçe_Shape")
 
-data <- readOGR(dsn = ".",layer = "gis_osm_buildings_a_free_1")
+sp_data <- readOGR(dsn = ".",layer = "il_sinir")
+
+#first give your data, then select color with col, backround with bg, linewidth wit lw
+plot(sp_data, col="white", bg="red", lwd= 2)
+
+#for better plots, we're going to use ggplot,
+#but to use ggplot, we must convert our spatial data to a regular dataframe,
+#we do this with fortify.
+library(ggplot2)
+df <- fortify(sp_data)
+
+ggplot()+
+  geom_polygon(data = df, aes(x = long, y=lat, group = group))+
+  theme_bw()
+
+#so now, we succesfuly plot our shapefile using ggplot. 
